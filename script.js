80ae7ebcbabc1e0a2b923ec0abfeef84568b05e0
ghadahelou859-dot <ghadahelou859@gmail.com>
2026-09-18 15:28:21 +0300

    Reveal mobile videos only after play

--- script.js
+++ script.js
@@ -214,19 +214,34 @@ function stopGrowthMusic(){
 }
 
 coverStartBtn.onclick=()=>showScreen('videoScreen');
-playIntroBtn.onclick=async()=>{playIntroBtn.classList.add('hidden');introVideo.currentTime=0;try{await introVideo.play();}catch{playIntroBtn.classList.remove('hidden')}};
+playIntroBtn.onclick=async()=>{
+  playIntroBtn.classList.add('hidden');
+  introVideo.classList.remove('mobile-awaiting-play');
+  introVideo.currentTime=0;
+  try{await introVideo.play();}
+  catch{
+    introVideo.classList.add('mobile-awaiting-play');
+    playIntroBtn.classList.remove('hidden');
+  }
+};
 introVideo.onended=()=>goGrowthBtn.classList.remove('hidden');
 introVideo.onerror=()=>goGrowthBtn.classList.remove('hidden');
 goGrowthBtn.onclick=()=>showScreen('growthScreen');
 skipIntroBtn.onclick=()=>{
   try{introVideo.pause();}catch(e){}
+  if(isPortraitMobile())growthVideo.classList.add('mobile-awaiting-play');
   showScreen('growthScreen');
 };
 playGrowthBtn.onclick=async()=>{
   playGrowthBtn.classList.add('hidden');
+  growthVideo.classList.remove('mobile-awaiting-play');
   growthVideo.currentTime=0;
   try{startGrowthMusic();await growthVideo.play();}
-  catch{stopGrowthMusic();playGrowthBtn.classList.remove('hidden')}
+  catch{
+    stopGrowthMusic();
+    growthVideo.classList.add('mobile-awaiting-play');
+    playGrowthBtn.classList.remove('hidden');
+  }
 };
 growthVideo.onended=()=>{stopGrowthMusic();showPartsBtn.classList.remove('hidden')};
 growthVideo.onerror=()=>{stopGrowthMusic();showPartsBtn.classList.remove('hidden')};
@@ -461,6 +476,10 @@ function playClap(){
 }
 
 syncResponsiveAssets();
+if(isPortraitMobile()){
+  introVideo.classList.add('mobile-awaiting-play');
+  growthVideo.classList.add('mobile-awaiting-play');
+}
 recordView();
 refreshLike();
 layoutAllMappedElements();
